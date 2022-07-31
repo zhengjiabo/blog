3d2e3ce35b3b3d4db1252e042e72c33f83a3af8f
Jiabo Zheng <804552226@qq.com>
2022-08-01 01:13:45 +0800

    docs(docker): docker - 3 | 极简项目的 docker 部署  问题验证

---
+++ docs/views/docker/220730/entrypoint/docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo "*****************"
+echo "**************"
+echo "***********"
+echo "run in custom"
+
+$@
+# exec "$@"
+
+echo "run in custom"
+echo "***********"
+echo "**************"
+echo "*****************"
