c21bb1ad688aaef6a65231f7aa86c73c7143b4a4
Jiabo Zheng <804552226@qq.com>
2023-03-26 18:41:20 +0800

    chore: blog

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -6,26 +6,16 @@ import sidebar from './generate/sidebar.json' assert {type: 'json'};
 export default defineConfig({
   title: "Knowledge Framework",
   description: "知识框架梳理",
+  base: '/blog/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
       ...nav
     ],
-
-    // sidebar: [
-    //   {
-    //     text: 'Examples',
-    //     items: [
-    //       { text: 'Markdown Examples', link: '/markdown-examples' },
-    //       { text: 'Runtime API Examples', link: '/api-examples' }
-    //     ]
-    //   }
-    // ],
     sidebar,
-
     socialLinks: [
       { icon: 'github', link: 'https://github.com/zhengjiabo/blog' }
-    ]
+    ],
   }
 })
